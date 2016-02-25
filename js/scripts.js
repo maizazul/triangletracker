b8212283619b1b9319cc02f693fe0c6ce156e8d3
Danny Pineda and Ming Wen <mwen82@gmail.com>
2016-02-24 20:07:18 -0800

    Finish Project

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,19 @@
-var triangletracker = function(a,b,c) {
-  var sumab = a+b;
-  var sumbc = b+c;
-  var sumac = a+c;
-     if ((sumab <= c) || (sumbc <= a) || (sumac <= b)){
-        return "not a triangle"
-     }
-     else if ((a === b) && (b === c) && (c === a)){
-        return "equilateral";
-     } else if ((a === b) && (c !== a)){
-        return "isosceles"
-     } else if ((a === c) && (b !== c)) {
-        return "isosceles"
-     } else if ((a !== b) && (b === c)){
-        return "isosceles"
-     } else if ((a !== b) && (b !== c)){
-        return "scalene"
-     }
-  };
+var factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+
+  return a * factorial(a-1);
+  }
+
 
 $(document).ready(function() {
   $("form#rate").submit(function(event) {
-    var a = parseInt($("input#side1").val());
-    var b = parseInt($("input#side2").val());
-    var c = parseInt($("input#side3").val());
+    var a = parseInt($("input#a").val());
 
-    var result = triangletracker(a,b,c);
+    var result = factorial(a);
 
-    $("#triangletype").text(result);
+    $("#factorial").text(result);
 
     $("#result").show();
     event.preventDefault();
