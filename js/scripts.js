f71e477b2a04e16a3bb6b758f1ebb36c83fb7977
Danny Pineda and Ming Wen <dcorn13@gmail.com>
2016-02-24 18:14:35 -0800

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(function() {
+  $("form#rate").submit(function(event) {
+    var rating1 = parseInt($("input#rating1").val());
+    var rating2 = parseInt($("input#rating2").val());
+    var rating3 = parseInt($("input#rating3").val());
+    var times = parseInt($("#timesyouvacation").val());
+
+    if () {
+
+    } else if () {
+
+    }
+    else {
+
+    }
+
+
+    $("#result").fadeIn().show();
+
+      event.preventDefault();
+  });
+});
